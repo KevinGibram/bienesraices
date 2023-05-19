a415090a5af3b8bcd8076b3f00f8e964e6d9c535
KevinGibram <kevinrincon0911@gmail.com>
2023-05-19 01:44:27 -0500

    script del dark-mode

--- src/js/app.js
+++ src/js/app.js
@@ -5,14 +5,43 @@ document.addEventListener('DOMContentLoaded', function(){
     imgDar();
 });
 
-function imgDar(){
-    const darkBoton = document.querySelector('.dark-mode-boton');
+function imgDar(){ //dark_mode
+    const dark = window.matchMedia('(prefer-color-schene: dark)');
+
+    if(dark.matches){
+        document.body.classList.add('darkMode');
+    }else{
+        document.body.classList.remove('darkMode')
+    }
+
+    dark.addEventListener('change', function(){
+        if(dark.matches){
+        document.body.classList.add('darkMode');
+    }else{
+        document.body.classList.remove('darkMode')
+    }
+    })
+}
+
+const darkBoton = document.querySelector('.dark-mode-boton');
 
     darkBoton.addEventListener('click', function(){
         
         document.body.classList.toggle('darkMode');
+
+        if(document.body.classList.contains('darkMode')){
+            localStorage.setItem('modo-oscuro','true');
+        } else {
+            localStorage.setItem('modo-oscuro','false');
+        }
     });
-}
+
+    if (localStorage.getItem('modo-oscuro') === 'true') {
+        document.body.classList.add('darkMode');
+    } else {
+        document.body.classList.remove('darkMode');
+    }
+
 
 
 
