6190ea0dcc065906db7b92879975bc8302eee734
KevinGibram <kevinrincon0911@gmail.com>
2023-05-21 23:18:00 -0500

    Agregando apartado de Admin

---
+++ admin/propiedades/borrar.php
@@ -0,0 +1,12 @@
+<?php
+    require('../../includes/funciones.php');
+    incluteTemplate('header');
+?>
+
+    <main class=" seccion">
+        <h1>Crear</h1>
+    </main>
+
+<?php
+    incluteTemplate('footer')
+?>
